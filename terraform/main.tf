6b71e5a14e16d88357f54a7aa44e54efcd1d595f
nickzeleniuk <nickzeleniuk90dev@gmail.com>
2025-02-08 18:41:11 +0000

    formatting and modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,8 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-
-  ports {
-    internal = 80
-    external = 8000
-  }
+module "nginx" {
+  source = "./modules"
+  container_name = "tutorial"
+  image_name = "nginx"
 }
