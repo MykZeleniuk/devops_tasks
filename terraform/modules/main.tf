05d04b03fd5098ee6302753e61c6e5d0cc87c28c
nickzeleniuk <nickzeleniuk90dev@gmail.com>
2025-02-08 19:42:11 +0000

    fix 03

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+  
+  required_version = ">=0.12"
+}
+
 resource "docker_image" "nginx" {
   name         = var.image_name
   keep_locally = false
